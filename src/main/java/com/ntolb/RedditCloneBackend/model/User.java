37eef1b705224da7e085e0059300eaf9581c5323
Nate Tolbert <ntolbertu85@gmail.com>
2020-08-24 13:03:48 -0400

    BUILD: 08242020 -- ...model.User.java -- Added properties and annotations to User class;

--- src/main/java/com/ntolb/RedditCloneBackend/model/User.java
+++ src/main/java/com/ntolb/RedditCloneBackend/model/User.java
@@ -1,9 +1,41 @@
 package com.ntolb.RedditCloneBackend.model;
 
+import java.time.Instant;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
 
-	public User() {
-		// TODO Auto-generated constructor stub
-	}
-
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@NotBlank(message="Unique user ID is required.")
+	private String username;
+	@NotBlank(message="Encrypted password is required.")
+	private String password;
+	@NotBlank(message="User's first and last name are required.")
+	private String fName;
+	@NotBlank(message="User's first and last name are required.")
+	private String lName;
+	@Email
+	@NotEmpty(message="Valid email address is required.")
+	private String email;
+	private Instant createdDate;
+	private boolean enabled;
 }
