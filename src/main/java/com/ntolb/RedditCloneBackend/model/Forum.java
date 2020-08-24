d72ad48afc29e68f2b9fea5e5397b6bfc6ad3088
Nate Tolbert <ntolbertu85@gmail.com>
2020-08-24 10:41:28 -0400

    08242020 -- ...model.Forum.java -- Started work on model class Forum; Added all entity scope annotations;

--- src/main/java/com/ntolb/RedditCloneBackend/model/Forum.java
+++ src/main/java/com/ntolb/RedditCloneBackend/model/Forum.java
@@ -1,9 +1,19 @@
 package com.ntolb.RedditCloneBackend.model;
 
+import javax.persistence.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Forum {
 
-	public Forum() {
-		// TODO Auto-generated constructor stub
-	}
+	
 
 }
