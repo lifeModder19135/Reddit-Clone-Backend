5a324b4f0cd8ecf0a9a5331bea1e6fe62378f3c1
Nate Tolbert <ntolbertu85@gmail.com>
2020-08-24 11:23:06 -0400

    ...data.Forum.java -- Added properties to Forum class; Added field / property scoped JPA and validation annotations to Forum class properties to define how forum objects should be validated and persist to the database;

--- src/main/java/com/ntolb/RedditCloneBackend/model/Forum.java
+++ src/main/java/com/ntolb/RedditCloneBackend/model/Forum.java
@@ -1,6 +1,16 @@
 package com.ntolb.RedditCloneBackend.model;
 
+import java.time.Instant;
+import java.util.List;
+
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,7 +23,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Forum {
-
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	@NotBlank(message="Forum name is required")
+	private String name;
+	@NotBlank(message="Forum description is required")
+	private String description;
+	@OneToMany(fetch=FetchType.LAZY)
+	private List<Post> posts;
+	private Instant createdDate;
+	@ManyToOne(fetch=FetchType.LAZY)
+	private User user;
 
 }
