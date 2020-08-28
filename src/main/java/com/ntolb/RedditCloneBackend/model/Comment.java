7dc05a015123ade2b7968511cf9b4858b526bd59
Nate Tolbert <ntolbertu85@gmail.com>
2020-08-28 10:48:30 -0400

    BUILD: 08282020 -- model.Comment.java -- Added Added properties to the Comment class (id, text, post); Added class-wide annotations, along with annotations for these properties;

--- src/main/java/com/ntolb/RedditCloneBackend/model/Comment.java
+++ src/main/java/com/ntolb/RedditCloneBackend/model/Comment.java
@@ -8,6 +8,7 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotEmpty;
 
@@ -29,6 +30,11 @@ public class Comment {
 	@NotEmpty(message="Comment text cannot be empty.")
 	private String text;
 	@ManyToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name="postId", referencedColumnName="postId")
 	private Post post;
+	private Instant createdDate;
+	@ManyToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name="username", referencedColumnName="username")
+	private User user;
 
 }
