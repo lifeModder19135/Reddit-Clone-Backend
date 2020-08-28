27f404fd28a55248c2a57ceb8d33bf03d5b0e82e
Nate Tolbert <ntolbertu85@gmail.com>
2020-08-28 15:17:06 -0400

    FIX: 08282020 -- PostRepository.java -- Added a forgotten import statement for Post class;

--- src/main/java/com/ntolb/RedditCloneBackend/repository/PostRepository.java
+++ src/main/java/com/ntolb/RedditCloneBackend/repository/PostRepository.java
@@ -2,6 +2,8 @@ package com.ntolb.RedditCloneBackend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.ntolb.RedditCloneBackend.model.Post;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
 
 }
