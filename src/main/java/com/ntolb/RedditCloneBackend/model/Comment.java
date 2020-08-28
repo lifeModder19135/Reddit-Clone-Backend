ddd7c7ab78d42b640c3201cc58ea2af797724d87
Nate Tolbert <ntolbertu85@gmail.com>
2020-08-28 10:36:06 -0400

    BUILD: 08242020 -- .model.Comment.java -- Added properties to the Comment class (id, text, post); Added class-wide annotations, along with annotations for these three properties;

--- src/main/java/com/ntolb/RedditCloneBackend/model/Comment.java
+++ src/main/java/com/ntolb/RedditCloneBackend/model/Comment.java
@@ -1,9 +1,34 @@
 package com.ntolb.RedditCloneBackend.model;
 
+import java.time.Instant;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotEmpty;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Comment {
 
-	public Comment() {
-		// TODO Auto-generated constructor stub
-	}
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	@NotEmpty(message="Comment text cannot be empty.")
+	private String text;
+	@ManyToOne(fetch=FetchType.LAZY)
+	private Post post;
 
 }
