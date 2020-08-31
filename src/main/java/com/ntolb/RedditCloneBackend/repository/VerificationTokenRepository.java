b6ef87998b98840e1a0d114251d19ca0828a4d63
Nate Tolbert <ntolbertu85@gmail.com>
2020-08-31 10:59:42 -0400

    FIX: 08312020 -- /com.ntolb.RedditCloneBackend.repository/ -- Added annotation @Repository to VerificationTokenRepository and VoteRepository interfaces

--- src/main/java/com/ntolb/RedditCloneBackend/repository/VerificationTokenRepository.java
+++ src/main/java/com/ntolb/RedditCloneBackend/repository/VerificationTokenRepository.java
@@ -1,9 +1,11 @@
 package com.ntolb.RedditCloneBackend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.ntolb.RedditCloneBackend.model.VerificationToken;
 
+@Repository
 public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
 
 }
