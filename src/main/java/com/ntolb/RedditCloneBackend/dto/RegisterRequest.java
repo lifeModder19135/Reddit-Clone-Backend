2db607d04721f4af6ef98cf44369d970e6312ef4
Nate Tolbert <ntolbertu85@gmail.com>
2020-09-03 21:07:28 -0400

    BUILD: 09032020 -- src/main/java/'''/dto/RegisterRequest.java -- added dto class; Objects will pass login info to be authorized;

---
+++ src/main/java/com/ntolb/RedditCloneBackend/dto/RegisterRequest.java
@@ -0,0 +1,16 @@
+package com.ntolb.RedditCloneBackend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+
+	private String username;
+	private String email;
+	private String password;
+	
+}
--- src/main/java/com/ntolb/RedditCloneBackend/model/User.java
+++ src/main/java/com/ntolb/RedditCloneBackend/model/User.java
@@ -3,8 +3,6 @@ package com.ntolb.RedditCloneBackend.model;
 import java.time.Instant;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
