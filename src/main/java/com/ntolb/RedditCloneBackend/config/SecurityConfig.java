86602925fc29fa8352605bf85d2567f9b9a1cc8a
Nate Tolbert <ntolbertu85@gmail.com>
2020-09-03 22:12:22 -0400

    BUILD: 09032020 -- /config/SecurityConfig.java && /service/AuthService.java -- Added PasswordEncoder bean and injected into service to hash password string before storage/use by application;

--- src/main/java/com/ntolb/RedditCloneBackend/config/SecurityConfig.java
+++ src/main/java/com/ntolb/RedditCloneBackend/config/SecurityConfig.java
@@ -1,6 +1,9 @@
 package com.ntolb.RedditCloneBackend.config;
 
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
@@ -18,5 +21,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 					.permitAll()
 					.anyRequest()
 					.authenticated();
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
--- src/main/java/com/ntolb/RedditCloneBackend/service/AuthService.java
+++ src/main/java/com/ntolb/RedditCloneBackend/service/AuthService.java
@@ -2,6 +2,8 @@ package com.ntolb.RedditCloneBackend.service;
 
 import java.time.Instant;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.ntolb.RedditCloneBackend.dto.RegisterRequest;
@@ -10,11 +12,14 @@ import com.ntolb.RedditCloneBackend.model.User;
 @Service
 public class AuthService {
 	
+	@Autowired
+	private PasswordEncoder encoder;
+	
 	public void signup(RegisterRequest request) {
 		User newUser = new User();
 		newUser.setUsername(request.getUsername());
 		newUser.setEmail(request.getEmail());
-		newUser.setPassword(request.getPassword());
+		newUser.setPassword(encoder.encode(request.getPassword()));
 		newUser.setCreatedDate(Instant.now()) ;
 		newUser.setEnabled(false);
 	}
