394f1c8e61f04761828677a4dbd6523ddcc7c985
Nate Tolbert <ntolbertu85@gmail.com>
2020-09-08 11:52:17 -0400

    BUILD: 09082020 -- '''/service/MailBuildService.java -- Added 'build(String message)' method to MailBuildService class; Method creates a Thymeleaf context object with a mailTemplate.html template wrapping the provided string message param; result: the message is injected into the mail template and the result is returned ready for injection of its own;

--- src/main/java/com/ntolb/RedditCloneBackend/service/MailBuildService.java
+++ src/main/java/com/ntolb/RedditCloneBackend/service/MailBuildService.java
@@ -1,5 +1,21 @@
 package com.ntolb.RedditCloneBackend.service;
 
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
 public class MailBuildService {
+	
+	private final TemplateEngine templateEngine;
+	
+	String build(String message) {
+		Context context =new Context();
+		context.setVariable("message", message);
+		return templateEngine.process("mailTemplate", context);
+	}
 
 }
