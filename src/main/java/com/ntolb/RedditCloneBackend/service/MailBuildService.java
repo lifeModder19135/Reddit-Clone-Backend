3a1af4799e57e0778517ba33ffcec361fc13e758
Nate Tolbert <ntolbertu85@gmail.com>
2020-09-24 04:41:46 -0400

    /test/java/ -- Changed package structure to match src/main/java/; src/main/resources/templates/mailTemplate.html -- added quotes around  variable; '''.service.VerificationEmailBuilder.java -- Added builder class to handle injection of messages into thymeleaf template;

--- src/main/java/com/ntolb/RedditCloneBackend/service/MailBuildService.java
+++
@@ -1,5 +0,0 @@
-package com.ntolb.RedditCloneBackend.service;
-
-public class MailBuildService {
-
-}
---
+++ src/main/java/com/ntolb/RedditCloneBackend/service/VerificationEmailBuilder.java
@@ -0,0 +1,21 @@
+package com.ntolb.RedditCloneBackend.service;
+
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class VerificationEmailBuilder {
+	
+	TemplateEngine engine;
+	
+	public String build(String message) {
+		Context context = new Context();
+		context.setVariable("message", message);
+		return engine.process("mailTemplate", context);
+	}
+
+}
