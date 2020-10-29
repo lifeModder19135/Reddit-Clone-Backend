a180d4603175ff1ae7b95cfb3b4f05f7bda8d726
Nate Tolbert <ntolbertu85@gmail.com>
2020-10-29 02:44:35 -0400

    BUILD: 10292020 -- UserController.java -- Added implementation to signup() method. Injects userService and calls its signup() method;

--- src/main/java/com/ntolb/RedditCloneBackend/controller/AuthController.java
+++ src/main/java/com/ntolb/RedditCloneBackend/controller/AuthController.java
@@ -1,24 +1,30 @@
 package com.ntolb.RedditCloneBackend.controller;
 
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ntolb.RedditCloneBackend.config.SecurityConfig;
+
 import com.ntolb.RedditCloneBackend.dto.RegisterRequest;
 import com.ntolb.RedditCloneBackend.service.AuthService;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
+	
+	@Autowired
+	private AuthService service;
 
 	@PostMapping("/signup")
-	public void signup(@RequestBody RegisterRequest registerRequest) {
-			
-		//AuthService service = new AuthService(SecurityConfig.passwordEncoder(), );
-		//service.signup(registerRequest);		
+	public ResponseEntity<String> signup(@RequestBody RegisterRequest registerRequest) {
+		
+		service.signup(registerRequest);
+		return new ResponseEntity<String> ("Registration successful!", HttpStatus.OK);
+		
 	}
 	
 }
